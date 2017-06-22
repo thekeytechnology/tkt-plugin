7284aade7b769aa32138aaad556f67ca3b5442da
Karl Kröber <karl.kroeber@thekey.technology>
2017-06-22 11:17:05 +0200

    progress

--- utils/tk-mail.php
+++ utils/tk-mail.php
@@ -1,11 +1,19 @@
 <?php
 
-function tkSendMail($email, $subject, $content)
+function tkSendMail($email, $subject, $content, $replyTo)
 {
-    wp_mail($email, $subject, $content, array("Content-type" => "text/html", "Reply-To", "molly@blinkist.com"));
+    wp_mail($email, $subject, $content, array("Content-type" => "text/html", "Reply-To" => $replyTo));
 }
 
-function wpse27856_set_content_type(){
+function wpse27856_set_content_type()
+{
     return "text/html";
 }
-add_filter( 'wp_mail_content_type','wpse27856_set_content_type' );
+
+add_filter('wp_mail_content_type', 'wpse27856_set_content_type');
+
+add_filter("retrieve_password_message", function ($message, $key, $user_login, $user_data) {
+    $message = str_replace("<", "", $message);
+    $message = str_replace(">", "", $message);
+    return $message;
+}, 10, 4);
