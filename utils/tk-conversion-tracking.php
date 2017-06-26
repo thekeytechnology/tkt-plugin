a2bed01782d57767810e709ebb439a8990002a24
Rafael David Scholl Minino <rdsm@thekey.technology>
2017-06-26 17:21:42 +0200

    Added Facebook tracking to tk-conversion-tracking \n Tracking now happens when the mail is sent (before: when the button was pressed) \n Updated responsive-less in theme files

--- utils/tk-conversion-tracking.php
+++ utils/tk-conversion-tracking.php
@@ -1,16 +1,21 @@
 <?php
 
+/*
+ * Put <input type="hidden" name="tk-conversion-action" value="name_of_conversion"/> into forms where conversion tracking is supposed to occur.
+ * */
+
 function tkCF7ConversionTracking()
 {
     ?>
     <script type="text/javascript">
-        document.addEventListener('wpcf7submit', function (event) {
+        document.addEventListener('wpcf7mailsent', function (event) {
             var inputs = event.detail.inputs;
 
             for (var i = 0; i < inputs.length; i++) {
                 if ('tk-conversion-action' === inputs[i].name) {
                     __gaTracker && __gaTracker('send', 'event', 'Conversion', input[i].value);
                     ga && ga('send', 'event', 'Conversion', input[i].value);
+                    fbq && fbq('trackCustom', 'Conversion', {type: input[i].value});
                 }
 
             }
