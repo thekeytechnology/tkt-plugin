d615b302fa1431b8f19c4af99c8f74f7ed3280d6
Karl Kröber <karl.kroeber@thekey.technology>
2017-08-07 16:45:36 +0200

    added excerpt twig filter

---
+++ tkt_theme_files/template-tk-content.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Template Name: TKT Content
+ * Description: TKT Content page template
+ *
+ * @package tkt
+ * @author the key technology
+ */
+
+get_header();
+?>
+
+    <!-- #Content -->
+    <div id="Content">
+        <div class="content_wrapper clearfix">
+
+
+
+            <?php ob_start(); ?>
+
+
+            <!-- .sections_group -->
+            <div class="sections_group">
+
+                <div class="entry-content tk-content-padding" itemprop="mainContentOfPage">
+
+                    <?php
+                    while (have_posts()) {
+                        the_post();                            // Post Loop
+                        mfn_builder_print(get_the_ID());    // Content Builder & WordPress Editor Content
+                    }
+                    /*if (function_exists("get_yuzo_related_posts")) {
+                        get_yuzo_related_posts();
+                    }*/
+                    ?>
+
+                </div>
+
+                <?php if (mfn_opts_get('page-comments')): ?>
+                    <div class="section section-page-comments">
+                        <div class="section_wrapper clearfix">
+
+                            <div class="column one comments">
+                                <?php comments_template('', true); ?>
+                            </div>
+
+                        </div>
+                    </div>
+                <?php endif; ?>
+
+            </div>
+
+            <?php $output = ob_get_clean( );
+
+            $output = add_ids_to_header_tags($output); ?>
+
+
+            <!-- .four-columns - sidebar -->
+            <?php get_sidebar("Inhalt"); ?>
+
+
+            <?php echo $output; ?>
+
+
+        </div>
+    </div>
+
+<?php get_footer();
+
+// Omit Closing PHP Tags
--- twig/filters/tk-wp-fields.php
+++ twig/filters/tk-wp-fields.php
@@ -15,6 +15,10 @@ function tkAddWpFieldFilters(TkTemplate $tkTwig)
         return get_user_by("ID", $authorId);
     });
 
+    $tkTwig->addFilter("wpexcerpt", function (WP_Post $item) {
+        return $item->post_excerpt;
+    });
+
     $tkTwig->addFilter("wpmeta", function ($item, $metaKey, $single = true) {
         return tkWpMeta($item, $metaKey, $single);
     });
