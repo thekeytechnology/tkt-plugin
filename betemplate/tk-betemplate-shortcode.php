957a67b417c577b67b0844337a4c03bd44591c85
Rafael David Scholl Minino <rdsm@thekey.technology>
2017-09-29 17:03:00 +0200

    BeTemplate: nesting of the shortcode itself is now optional (defaults to false) because using it in the_content with nesting enabled breaks the page

--- betemplate/tk-betemplate-shortcode.php
+++ betemplate/tk-betemplate-shortcode.php
@@ -1,28 +1,51 @@
 <?php
+
 function tkGetBeThemeTemplate($atts){
     $args = shortcode_atts(array(
         "id" => "",
-        "stripdown" => ""
+        "stripdown" => "",
+        "enable_nesting" => false
     ), $atts, "tk-betemplate");
+    
+
+    //prevent nesting this shortcode unless explicitly enabled
+    //WARNING: do not enable nesting if the shortcode is used in the_content
+    //mfn_builder_print always calls the_content
+    //this can only be prevented with the mfn-post-hide-content flag, which it reads directly from post meta
+    if(!$args["enable_nesting"]){
+        remove_shortcode("tk-betemplate");
+    }
 
     //capture muffin builder output
     ob_start();
     mfn_builder_print($args["id"]);
     $output = ob_get_clean();
 
+    //re-enable the shortcode if it was disabled earlier
+    if(!$args["enable_nesting"]){
+        add_shortcode("tk-betemplate", "tkGetBeThemeTemplate");
+    }
+
+
     //remove the_content (less of a hassle than messing with post meta)
     $output = trim($output);
     $output = preg_replace( '/(.*?)<div class="section the_content.*?"?\s*>?\s*<\/div>?\s*$/sD' , '$1' , $output );
 
     $stripdownLevel = 0;
-    if(($args["stripdown"]==="wrap"))
+    if(($args["stripdown"]==="wrap")){
         $stripdownLevel = 1;
-    if(($args["stripdown"]==="element"))
+    }
+    if(($args["stripdown"]==="element")){
         $stripdownLevel = 2;
-    if(($args["stripdown"]==="prettytext")) //this setting applies visual editor auto-paragraphs
+    }
+    //this setting applies visual editor auto-paragraphs
+    if(($args["stripdown"]==="prettytext")){
         $stripdownLevel = 3;
-    if(($args["stripdown"]==="plaintext")) //this setting reduces to just the text
+    }
+    //this setting reduces to just the text
+    if(($args["stripdown"]==="plaintext")){
         $stripdownLevel = 4;
+    }
 
     //caution: stripdown assumes that only one section and one wrap are used
     //text stripdown assumes that only one column or visual editor element are used (in addition to the above)
@@ -57,4 +80,5 @@ function tkGetBeThemeTemplate($atts){
 
     return $output;
 }
+
 add_shortcode("tk-betemplate", "tkGetBeThemeTemplate");
--- tkt.php
+++ tkt.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: TKT Less / Gulp / Twig
 Plugin URI:  https://www.thekey.technology
-Version:     5.0
+Version:     6.0
 Author:      the key technology
 Author URI:  https://www.thekey.technology
 License:     proprietary
