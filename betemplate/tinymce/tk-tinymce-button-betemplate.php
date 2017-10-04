6eab0534605ed3ee3126f1a07df462fbe87b82f4
Rafael David Scholl Minino <rdsm@thekey.technology>
2017-10-04 10:27:14 +0200

    fix: renamed betemplate tinymce functions to avoid conflicts

--- betemplate/tinymce/tk-tinymce-button-betemplate.php
+++ betemplate/tinymce/tk-tinymce-button-betemplate.php
@@ -1,19 +1,19 @@
 <?php
-function enqueue_plugin_scripts($plugin_array)
+function tkBetemplateEnqueuePluginScripts($plugin_array)
 {
     //enqueue TinyMCE plugin script with its ID.
     $plugin_array["betemplate_button_plugin"] =  plugin_dir_url( __FILE__ ) . "button-betemplate.js";
     return $plugin_array;
 }
 
-add_filter("mce_external_plugins", "enqueue_plugin_scripts");
+add_filter("mce_external_plugins", "tkBetemplateEnqueuePluginScripts");
 
 
-function register_buttons_editor($buttons)
+function tkBetemplateRegisterButtonsEditor($buttons)
 {
     //register buttons with their id.
     array_push($buttons, "betemplate");
     return $buttons;
 }
 
-add_filter("mce_buttons", "register_buttons_editor");
+add_filter("mce_buttons", "tkBetemplateRegisterButtonsEditor");
--- tkt.php
+++ tkt.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: TKT Less / Gulp / Twig
 Plugin URI:  https://www.thekey.technology
-Version:     6.0
+Version:     7.0
 Author:      the key technology
 Author URI:  https://www.thekey.technology
 License:     proprietary
