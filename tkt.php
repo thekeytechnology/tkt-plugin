4fe520386a4187026988a77b18867cdf0234e623
Rafael David Scholl Minino <rdsm@thekey.technology>
2017-11-28 16:48:49 +0100

    updated gitignore

--- tkt.php
+++ tkt.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: TKT Less / Gulp / Twig / WP Utilities
 Plugin URI:  https://www.thekey.technology
-Version:     18.0
+Version:     18.1
 Author:      the key technology
 Author URI:  https://www.thekey.technology
 License:     proprietary
