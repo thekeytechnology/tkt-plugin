fe28819a470c74062adf656e9fee8c7e138d3ec5
Rafael David Scholl Minino <rdsm@thekey.technology>
2018-02-08 18:27:32 +0100

    tk-variable-parameter: added shortcode attribute for html tag attributes

--- shortcodes/tk-variable-parameter.php
+++ shortcodes/tk-variable-parameter.php
@@ -4,6 +4,7 @@ function tkVariableParameter($atts, $defaultContent = null)
 {
     $tag = s("tag", $atts, "");
     $parameter = s("parameter", $atts, "campaign");
+    $tag_attributes = s("tag_attributes", $atts, "");
 
     $content = $defaultContent;
     if (isset($_GET[$parameter])) {
@@ -11,7 +12,7 @@ function tkVariableParameter($atts, $defaultContent = null)
         $content = base64_decode(urldecode($campaign));
     }
 
-    return empty($tag) ? $content : "<$tag>$content</$tag>";
+    return empty($tag) ? $content : "<$tag $tag_attributes>$content</$tag>";
 }
 
 add_shortcode("tk-variable-parameter", "tkVariableParameter");
