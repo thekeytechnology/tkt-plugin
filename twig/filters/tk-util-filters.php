e29bf1bae8e213cf28613cbe1a5d33017ab904c2
Daniel Rockenstein <daniel.rockenstein@thekey.technology>
2018-05-02 17:13:24 +0200

    Updated twig pms filters

--- twig/filters/tk-util-filters.php
+++ twig/filters/tk-util-filters.php
@@ -8,6 +8,16 @@ function tkAddUtilFilters(TkTemplate $tkTwig)
     $tkTwig->addFilter("tkToArray", function($item) {
         return (array) $item;
     });
+
+    $tkTwig->addFilter("tkIntersect", function ($array1, $array2) {
+        return array_intersect($array1, $array2);
+    });
+
+    $tkTwig->addFilter("tkContains", function ($array1, $array2) {;
+        return empty(array_intersect($array1, $array2));
+    });
+
+
 }
 
 
