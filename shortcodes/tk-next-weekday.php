44d5df178fd1f6d744f7b5c1cd37ad7d19f223ad
Daniel Rockenstein <daniel.rockenstein@thekey.technology>
2018-05-07 12:36:57 +0200

    added tk-next-weekday shortcode

--- shortcodes/tk-next-weekday.php
+++ shortcodes/tk-next-weekday.php
@@ -1,7 +1,8 @@
 
 <?php
+if (!function_exists("tkNextWeekday")) {
 
-if (!function_exists("tk-next-week-day")) {
+    echo "<h1 style='display: none' class='tk-test'></h1>";
     function tkNextWeekday($atts) {
 
 
@@ -11,8 +12,9 @@ if (!function_exists("tk-next-week-day")) {
             ), $atts, 'tk-next-weekday' );
 
 
-        return date("d-m-Y", strtotime("next " . $atts['day']));
+        return date("d.m.Y", strtotime("next sunday"));
     }
 
     add_shortcode("tk-next-weekday", "tkNextWeekday");
+
 }
