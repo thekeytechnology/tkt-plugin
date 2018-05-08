89f519a72463be63f76fc80e1ba144e167ea67e5
Rafael David Scholl Minino <rdsm@thekey.technology>
2018-05-08 14:57:19 +0200

    removed errant test code

--- shortcodes/tk-next-weekday.php
+++ shortcodes/tk-next-weekday.php
@@ -2,19 +2,13 @@
 <?php
 if (!function_exists("tkNextWeekday")) {
 
-    echo "<h1 style='display: none' class='tk-test'></h1>";
     function tkNextWeekday($atts) {
-
-
         $atts = shortcode_atts(
             array(
                 'day' => 'Sunday',
             ), $atts, 'tk-next-weekday' );
-
-
         return date("d.m.Y", strtotime("next sunday"));
     }
 
     add_shortcode("tk-next-weekday", "tkNextWeekday");
-
 }
