7ec8e9559a6f1e7b78a7e209831237145636d013
Daniel Rockenstein <daniel.rockenstein@thekey.technology>
2018-05-24 15:03:15 +0200

    Remove all empty <p>s before and after content is added using tk-betemplate

--- betemplate/tk-betemplate-shortcode.php
+++ betemplate/tk-betemplate-shortcode.php
@@ -93,6 +93,22 @@ function tkGetBeThemeTemplate($atts){
         }
     }
 
+
+    // clean up p tags around block elements
+    $output = preg_replace(array(
+        '#<p>\s*<(div|aside|section|article|header|footer)#',
+        '#</(div|aside|section|article|header|footer)>\s*</p>#',
+        '#</(div|aside|section|article|header|footer)>\s*<br ?/?>#',
+        '#<(div|aside|section|article|header|footer)(.*?)>\s*</p>#',
+        '#<p>\s*</(div|aside|section|article|header|footer)#',
+    ), array(
+        '<$1',
+        '</$1>',
+        '</$1>',
+        '<$1$2>',
+        '</$1',
+    ), $output);
+
     return $output;
 }
 
