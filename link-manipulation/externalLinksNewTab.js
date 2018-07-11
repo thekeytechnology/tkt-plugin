1e430763fdd160a8d81007971b2426abc5cebf55
Karl Kröber <karl.kroeber@thekey.technology>
2018-07-11 19:59:59 +0200

    fixed issue with javascript for link openers

--- link-manipulation/externalLinksNewTab.js
+++ link-manipulation/externalLinksNewTab.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function () {
+jQuery(document).ready(function ($) {
     $('a')
         .filter('[href^="http"], [href^="//"]')
         .not('[href*="' + window.location.host + '"]')
