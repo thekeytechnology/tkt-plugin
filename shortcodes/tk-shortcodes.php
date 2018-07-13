a7490eb26bd2aae5dda55832930ceb53d2b27b9d
Rafael David Scholl Minino <rdsm@thekey.technology>
2018-07-13 10:46:18 +0200

    UPT: added traffic-source-id (machine-readable traffic source); added source-based replacement (mainly for mailto/tel links); added tk-option shortcode

--- shortcodes/tk-shortcodes.php
+++ shortcodes/tk-shortcodes.php
@@ -9,10 +9,16 @@ function tkAttribute($atts)
 
     return get_post_meta(get_queried_object_id(), $args["field"], $args["single"]);
 }
-add_shortcode("tk-attribute", function ($atts) {
+add_shortcode("tk-attribute", "tkAttribute");
 
+add_shortcode("tk-option", function($atts) {
+    $atts = shortcode_atts(array(
+        "option" => "",
+        "default" => "",
+        "do_shortcode" => false
+    ), $atts, "tk-option");
+    return $atts["do_shortcode"] ? do_shortcode(get_option($atts["option"], $atts["default"])) : get_option($atts["option"], $atts["default"]);
 });
-
 
 add_shortcode("tk-year", function () {
     return date("Y");
