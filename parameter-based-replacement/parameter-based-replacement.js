64cbd55dfeaac3d38b5b44ba4ef31f018d919e18
Daniel Rockenstein <daniel.rockenstein@thekey.technology>
2019-01-18 16:44:45 +0100

    pbr now only checks if the parameter is somewhere in the url

--- parameter-based-replacement/parameter-based-replacement.js
+++ parameter-based-replacement/parameter-based-replacement.js
@@ -3,10 +3,21 @@ jQuery(document).ready(function ($) {
     if (typeof pbr_list !== 'undefined') {
         pbr_list.forEach(function(pbr_replace_pair){
 
+
+
+
+            if (window.location.href.indexOf(pbr_replace_pair['parameter']) != -1) {
+                var regex = new RegExp(pbr_replace_pair['search'],'g');
+                document.body.innerHTML = document.body.innerHTML.replace(regex, pbr_replace_pair['replace']);
+            }
+
+
+            /*
+            For now we just check if the parameter exists, but ideally it would work like in this comment
             var queryParams = decodeURIComponent(window.location.search.substring(1));
             queryParams = queryParams.split('&');
 
-            /* map them from key=value to {key: value} */
+            /* map them from key=value to {key: value}
             var queryParamsArray = {};
             queryParams.forEach(function(queryParamString) {
 
@@ -19,13 +30,14 @@ jQuery(document).ready(function ($) {
 
             });
 
+
             if (queryParamsArray[pbr_replace_pair['parameter']] != undefined ) {
                 if (queryParamsArray[pbr_replace_pair['parameter']] == pbr_replace_pair['value'] || pbr_replace_pair['value'] == "*") {
                     var regex = new RegExp(pbr_replace_pair['search'],'g');
 
                     document.body.innerHTML = document.body.innerHTML.replace(regex, pbr_replace_pair['replace']);
                 }
-            }
+            }*/
 
         });
     }
