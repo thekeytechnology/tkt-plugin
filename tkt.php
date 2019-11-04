bcd641bdace15200b29a03fc17219ce424de4085
Daniel Rockenstein <daniel.rockenstein@thekey.technology>
2019-11-04 16:27:34 +0100

    prg shortcode

--- tkt.php
+++ tkt.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: TKT Less / Gulp / Twig / WP Utilities
 Plugin URI:  https://www.thekey.technology
-Version:     91
+Version:     92
 Author:      the key technology
 Author URI:  https://www.thekey.technology
 License:     proprietary
