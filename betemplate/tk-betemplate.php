f1f7958d2af02ffa465762bb89d5377690a36bc3
Daniel Rockenstein <daniel.rockenstein@thekey.technology>
2020-11-10 15:39:44 +0100

    added betheme betemplate backwards compatibility, because betheme devs are morons

--- betemplate/tk-betemplate.php
+++ betemplate/tk-betemplate.php
@@ -29,9 +29,12 @@ function tkInstallBeTemplate()
 }
 
 /** Adding backwards compatibility to mfn builder because betheme devs are mentally challenged */
-if (!function_exists("mfn_builder_print") && class_exists('Mfn_Builder_Front')) {
+if (!function_exists("mfn_builder_print")) {
     function mfn_builder_print($post_id, $content_field = false) {
-        $mfn_builder = new Mfn_Builder_Front($post_id, $content_field, true);
-        $mfn_builder->show();
+        if (class_exists("Mfn_Builder_Front")) {
+            $mfn_builder = new Mfn_Builder_Front($post_id, $content_field, true);
+            $mfn_builder->show();
+        }
+
     }
 }
